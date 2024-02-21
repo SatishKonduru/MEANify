27357829aced82594ad70cfb52cbb3b5d9337d8b
satish <satishkonduru0225@gmail.com>
2024-02-21 22:10:52 +0530

    installed cors DB connected

--- MEANify/backend/app.js
+++ MEANify/backend/app.js
@@ -1,13 +1,25 @@
-const express = require('express')
 require('dotenv').config()
+const express = require('express')
 const morgan = require('morgan')
 const bodyParser =  require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT
 const api = process.env.API_URL
 const app =  express()
+
 // app.use(express.json() )
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(morgan('tiny'))
+app.use(cors())
+
+mongoose.connect(process.env.CONNECTION_STRING, {dbName: process.env.DB_NAME}).then(() => {
+    console.log("Database Connected")
+})
+.catch(()=>{
+    console.log("DB Connection Fail...")
+})
 app.listen(PORT, ()=> {
     console.log(api)
     console.log("Server Listening at Port: ", PORT)
@@ -15,5 +27,33 @@ app.listen(PORT, ()=> {
 
 //to Check in Browser with localhost:3000
 app.get('/', (req, res) => {
-    res.status(200).send({message: "OK"})
+    res.status(200).json({message: "OK"})
+})
+
+//Products
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number
+})
+const Product = mongoose.model('product', productSchema)
+
+app.post(`${api}/products`, (req, res) => {
+    const data = req.body
+    const product = new Product({
+        name: data.name,
+        image: data.image,
+        countInStock: data.countInStock
+    })
+    product.save()
+            .then((createdProduct) => {
+              res.status(200).json(createdProduct)
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    error: err,
+                    success: false
+                })
+            })
+
 })
