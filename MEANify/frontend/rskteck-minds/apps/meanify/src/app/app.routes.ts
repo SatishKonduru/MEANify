b53f2a79ae2fe53bddd944278576f2ce88cc6406
satish <satishkonduru0225@gmail.com>
2024-02-26 11:54:50 +0530

    created ui, users, products, orders libraries

--- MEANify/frontend/rskteck-minds/apps/meanify/src/app/app.routes.ts
+++ MEANify/frontend/rskteck-minds/apps/meanify/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomePageComponent
+    },
+    {
+        path: 'products',
+        component: ProductListComponent
+    }
+];
---
+++ MEANify/frontend/rskteck-minds/apps/meanify/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rskteck-minds-footer',
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
